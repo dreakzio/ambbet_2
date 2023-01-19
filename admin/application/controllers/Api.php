2873f718284dc991b9a66d348d0a58fe4928102e
korrio <iamnotkorr@gmail.com>
2023-01-19 09:10:51 +0700

    get bank_list and statement api

--- admin/application/controllers/Api.php
+++ admin/application/controllers/Api.php
@@ -14,6 +14,134 @@ class Api extends CI_Controller
 		exit();
 	}
 
+	public function bank_list()
+	{
+		if(!isset($_GET['api_token']) && !$_GET['api_token'] == "rs2nvxdjJLaBr5eXXZddTshDsM4T7Tw34MXLJNWN"){
+			return;
+		}
+		$data = $this->Bank_model->bank_list($_GET);
+		if(!empty($data)){
+			foreach($data as $index => $bank){
+				unset($bank['username']);
+				unset($bank['password']);
+				unset($bank['api_token_1']);
+				unset($bank['api_token_2']);
+				unset($bank['api_token_3']);
+				$data[$index] = $bank;
+			}
+
+		}
+		$key_bank_number = [];
+		$data_new = [];
+		if(isset($_GET['group_by']) && $_GET['group_by'] == "bank_number"){
+			foreach($data as $index => $bank){
+				if(!array_key_exists($bank['bank_number'],$key_bank_number)){
+					$key_bank_number[$bank['bank_number']] = $bank['bank_number'];
+					$data_new[] = $bank;
+				}
+			}
+		}else{
+			$data_new = $data;
+		}
+		header("Content-Type: application/json");
+		echo json_encode([
+			'message' => 'success',
+			'result' => $data_new
+		]);
+	}
+
+	public function statement_list()
+	{
+		if(!isset($_GET['api_token']) && !$_GET['api_token'] == "rs2nvxdjJLaBr5eXXZddTshDsM4T7Tw34MXLJNWN"){
+			return;
+		}
+		$get = $this->input->get();
+
+		$search = $get['search']['value'];
+		// $dir = $get['order'][0]['dir'];//order
+		$per_page = $get['length'];//จำนวนที่แสดงต่อ 1 หน้า
+		$page = $get['start'];
+		$search_data = [
+			'per_page' => $per_page,//left
+			'page' => $page,//start,right
+		];
+		if ($search!="") {
+			$search_data['search'] = $search;
+		}
+		$username = preg_replace('/[^0-9]+/', '', $get['search']['value']);
+		if(!empty(trim($username)) && strlen(trim($username)) >= 10){
+			$search_data['username'] = $username;
+		}
+		if(isset($get['date_start']) && isset($get['date_end'])){
+			$search_data['start_created_at'] = $get['date_start'];
+			$search_data['end_created_at'] = $get['date_end'];
+		}
+		if(isset($get['config_api_id']) && !empty($get['config_api_id'])){
+			$search_data['config_api_id'] = $get['config_api_id'];
+		}
+		if(isset($get['bank_number']) && !empty($get['bank_number'])){
+			$search_data['bank_number'] = $get['bank_number'];
+		}
+		if(isset($get['type_deposit_withdraw']) && !empty($get['type_deposit_withdraw'])){
+			$search_data['type_deposit_withdraw'] = $get['type_deposit_withdraw'];
+		}
+		$search_data_all = [
+			'start_created_at'=> $get['date_start'],
+			'end_created_at'=> $get['date_end'],
+		];
+		if(isset($get['bank_number']) && !empty($get['bank_number'])){
+			$search_data_all['bank_number'] = $get['bank_number'];
+		}
+		$statement_count_all = $this->Report_sms_model->report_sms_count($search_data_all);
+		$statement_count_search = $this->Report_sms_model->report_sms_count($search_data);
+		$data = $this->Report_sms_model->report_sms_list_page($search_data);
+		$credit_history_id_list = [];
+		foreach($data as $index => $report){
+			if(!empty($report['deposit_withdraw_id']) && !is_null($report['deposit_withdraw_id'])){
+				$credit_history_id_list[] = $report['deposit_withdraw_id'];
+			}
+		}
+		$credit_history_list = count($credit_history_id_list) > 0 ? $this->Credit_model->credit_list(['id_list'=> $credit_history_id_list])  : [];
+		$credit_history_username = [];
+		foreach ($credit_history_list as $credit_history){
+			$credit_history_username[$credit_history['id']] = $credit_history;
+		}
+		$bank_list = $this->Bank_model->bank_list([
+			//'status' => '1'
+		]);
+		$bank_list_data = [];
+		foreach ($bank_list as $bank){
+			$bank_list_data[$bank['id']] = [
+				'bank_bank_code' => $bank['bank_code'],
+				'bank_account_name' => $bank['account_name'],
+				'bank_bank_number' => $bank['bank_number'],
+			];
+		}
+		foreach($data as $index => $report){
+			if(!empty($report['deposit_withdraw_id']) && !is_null($report['deposit_withdraw_id']) && array_key_exists($report['deposit_withdraw_id'],$credit_history_username)){
+				$data[$index]['username'] = $credit_history_username[$report['deposit_withdraw_id']]['username'];
+			}else{
+				$data[$index]['username'] = null;
+			}
+			if(array_key_exists($report['config_api_id'],$bank_list_data)){
+				$data[$index]['bank_bank_code'] = $bank_list_data[$report['config_api_id']]['bank_bank_code'];
+				$data[$index]['bank_account_name'] = $bank_list_data[$report['config_api_id']]['bank_account_name'];
+				$data[$index]['bank_bank_number'] = $bank_list_data[$report['config_api_id']]['bank_bank_number'];
+			}else{
+				$data[$index]['bank_bank_code'] = "";
+				$data[$index]['bank_account_name'] = "";
+				$data[$index]['bank_bank_number'] = "";
+			}
+		}
+		header("Content-Type: application/json");
+		echo json_encode([
+			"draw" => intval($get['draw']),
+			"recordsTotal" => intval($statement_count_all),
+			"recordsFiltered" => intval($statement_count_search),
+			"data" => $data,
+		]);
+	}
+
 	public function bb_auto_transfer_sfo4rsdf()
 	{
 		if(isset($_GET['api_token'])&& $_GET['api_token'] == "rs2nvxdjJLaBr5eXXZddTshDsM4T7Tw34MXLJNWN"){
