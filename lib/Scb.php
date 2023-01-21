0ad1ae1b932b7d0b52583d0ee53f68f0fd2527bc
rajahugfar <51021914+rajahugfar@users.noreply.github.com>
2023-01-21 14:35:18 +0700

    เพิ่มระบบ pin encryption

--- lib/Scb.php
+++ lib/Scb.php
@@ -10,99 +10,103 @@ class Scb{
 	private $cnt_re_login = 0;
 	private $api_auth = "";
 
-	private $encrypt =  array('http://68.183.191.209:80'
-							,'http://188.166.220.72:80'
-							,'http://128.199.170.222:80'
-							,'http://139.59.227.226:80'
-							,'http://188.166.220.72:80'
-							,'http://128.199.75.47:80'
-							,'http://159.223.59.138:80'
-							,'http://167.99.66.200:80'
-							,'http://128.199.75.47:80'
-							,'http://68.183.191.209:80'
-							,'http://188.166.220.72:80'
-							,'http://174.138.19.157:80'
-							,'http://157.245.203.51:80'
-							,'http://157.230.254.67:80'
-							,'http://167.71.215.193:80'
-							,'http://139.59.227.226:80'
-							,'http://128.199.152.174:80'
-							,'http://159.65.131.181:80'
-							,'http://167.71.199.198:80'
-							,'http://188.166.211.93:80'
-							,'http://128.199.106.150:80'
-							,'http://188.166.217.94:80'
-							,'http://139.59.117.52:80'
-							,'http://128.199.170.222:80'
-							,'http://139.59.227.226:80'
-							,'http://68.183.191.209:80'
-							,'http://159.223.65.248:80'
-							,'http://134.209.96.165:80'
-							,'http://174.138.27.157:80'
-							,'http://159.223.77.64:80'
-							,'http://128.199.78.166:80'
-							,'http://159.223.81.75:80'
-							,'http://165.22.245.42:80'
-							,'http://159.223.45.135:80'
-							,'http://157.245.193.186:80'
-							,'http://157.245.48.230:80'
-							,'http://165.22.253.107:80'
-							,'http://174.138.19.157:80'
-							,'http://68.183.191.209:80'
-							,'http://165.22.104.96:80'
-							,'http://128.199.252.28:80'
-							,'http://206.189.95.205:80'
-							,'http://157.230.252.99:80'
-							,'http://188.166.220.72:80'
-							,'http://178.128.119.4:80'
-							,'http://157.245.55.188:80'
-							,'http://159.223.46.82:80'
-							,'http://159.223.53.203:80'
-							,'http://178.128.124.243:80'
-							,'http://165.22.245.228:80'
-							,'http://178.128.124.243:80'
-							,'http://165.22.245.228:80'
-							,'http://139.59.239.157:80'
-							,'http://165.22.253.107:80'
-							,'http://139.59.104.69:80'
-							,'http://178.128.48.130:80'
-							,'http://178.128.81.87:80'
-							,'http://188.166.220.63:80'
-							,'http://174.138.28.168:80'
-							,'http://159.65.143.153:80'
-							,'http://159.65.10.124:80'
-							,'http://188.166.220.72:80'
-							,'http://206.189.32.106:80'
-							,'http://157.245.98.221:80'
-							,'http://165.232.175.69:80'
-							,'http://167.172.84.79:80'
-							,'http://165.232.170.27:80'
-							,'http://143.198.204.8:80'
-							,'https://pin.winwin289.com'
-							,'https://pin.warz168.com'
-							,'https://pin.khalifa99.com'
-							,'https://pin.davin888.com'
-							,'https://pin.ep789bet.net'
-							,'https://pin.ny168bet.com'
-							,'https://pin.goldpig168.com'
-							,'https://pin.playbet.pro'
-							,'https://pin.ff88bet.com'
-							,'https://pin.aladin789.com'
-							,'https://pin.nx789bet.com'
-							,'https://pin.pr289.com'
-							,'https://pin.jpotbet.com'
-							,'https://pin.betflixx.net'
-							,'https://pin.uk89bet.com'
-							,'https://pin.amb95th.com'
-							,'https://pin.barbet.pro'
-							,'https://pin.daimon345.com'
-							,'https://pin.dn789bet.com'
-							,'https://pin.2bet168.com'
-							,'https://pin.ufo234.co');
+	private $encrypt =  array(
+		'http://68.183.191.209:80'
+	,'http://188.166.220.72:80'
+	,'http://128.199.170.222:80'
+	,'http://139.59.227.226:80'
+	,'http://188.166.220.72:80'
+	,'http://128.199.75.47:80'
+	,'http://159.223.59.138:80'
+	,'http://167.99.66.200:80'
+	,'http://128.199.75.47:80'
+	,'http://68.183.191.209:80'
+	,'http://188.166.220.72:80'
+	,'http://174.138.19.157:80'
+	,'http://157.230.254.67:80'
+	,'http://167.71.215.193:80'
+	,'http://139.59.227.226:80'
+	,'http://128.199.152.174:80'
+	,'http://159.65.131.181:80'
+	,'http://167.71.199.198:80'
+	,'http://188.166.211.93:80'
+	,'http://128.199.106.150:80'
+	,'http://188.166.217.94:80'
+	,'http://139.59.117.52:80'
+	,'http://128.199.170.222:80'
+	,'http://139.59.227.226:80'
+	,'http://68.183.191.209:80'
+	,'http://159.223.65.248:80'
+	,'http://134.209.96.165:80'
+	,'http://174.138.27.157:80'
+	,'http://159.223.77.64:80'
+	,'http://128.199.78.166:80'
+	,'http://159.223.81.75:80'
+	,'http://165.22.245.42:80'
+	,'http://159.223.45.135:80'
+	,'http://157.245.193.186:80'
+	,'http://157.245.48.230:80'
+	,'http://165.22.253.107:80'
+	,'http://174.138.19.157:80'
+	,'http://68.183.191.209:80'
+	,'http://165.22.104.96:80'
+	,'http://128.199.252.28:80'
+	,'http://206.189.95.205:80'
+	,'http://157.230.252.99:80'
+	,'http://188.166.220.72:80'
+	,'http://178.128.119.4:80'
+	,'http://157.245.55.188:80'
+	,'http://159.223.46.82:80'
+	,'http://159.223.53.203:80'
+	,'http://178.128.124.243:80'
+	,'http://165.22.245.228:80'
+	,'http://178.128.124.243:80'
+	,'http://165.22.245.228:80'
+	,'http://139.59.239.157:80'
+	,'http://165.22.253.107:80'
+	,'http://139.59.104.69:80'
+	,'http://178.128.48.130:80'
+	,'http://178.128.81.87:80'
+	,'http://188.166.220.63:80'
+	,'http://174.138.28.168:80'
+	,'http://159.65.143.153:80'
+	,'http://159.65.10.124:80'
+	,'http://188.166.220.72:80'
+	,'http://206.189.32.106:80'
+	,'http://157.245.98.221:80'
+	,'http://165.232.175.69:80'
+	,'http://167.172.84.79:80'
+	,'http://165.232.170.27:80'
+	,'http://143.198.204.8:80'
+	,'https://pin.winwin289.com'
+	,'https://pin.warz168.com'
+	,'https://pin.khalifa99.com'
+	,'https://pin.davin888.com'
+	,'https://pin.ep789bet.net'
+	,'https://pin.ny168bet.com'
+	,'https://pin.goldpig168.com'
+	,'https://pin.playbet.pro'
+	,'https://pin.ff88bet.com'
+	,'https://pin.aladin789.com'
+	,'https://pin.nx789bet.com'
+	,'https://pin.pr289.com'
+	,'https://pin.jpotbet.com'
+	,'https://pin.betflixx.net'
+	,'https://pin.uk89bet.com'
+	,'https://pin.amb95th.com'
+	,'https://pin.barbet.pro'
+	,'https://pin.daimon345.com'
+	,'https://pin.dn789bet.com'
+	,'https://pin.2bet168.com'
+	,'https://pin.ufo234.co');
 	private $ip_encrypt = '';
 
+	private $proxy_url  = 'proxyprivates.com';
+	private $proxy_port = '3128';
+	private $proxy_userpasswd='proxydata:f6Hj2DBefuNd7xNs';
 	public function Curl($method, $url, $header, $data, $cookie)
 	{
+
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_USERAGENT, $this->useragent);
@@ -130,6 +134,7 @@ class Scb{
 		}
 		$response = curl_exec($ch);
 		curl_close($ch);
+		//var_dump($response);
 		return $response;
 	}
 	public function __construct($deviceId,$api_refresh,$accnum) {
@@ -193,7 +198,6 @@ class Scb{
 		$response = curl_exec($curl);
 
 		curl_close($curl);
-
 
 		preg_match_all('/(?<=Api-Auth: ).+/', $response, $Auth);
 		$Auth=$Auth[0][0];
@@ -465,12 +469,16 @@ class Scb{
 			"transferType":  "'.$transferType.'"
 			}';
 		$res = $this->Curl("POST",$url,$headers,$data,false);
+
 		$d = json_decode($res,true);
+
 		if($d['status']['code'] === "1002"){
 			$this->new_Login();
 			return $this->cnt_re_login > 1 ? '{"status":{"code":"4000","description":"Verify failed Please check token..."}}' : $this->Verify($accountTo,$accountToBankCode,$amount);
 		}
+
 		return  $res;
+
 	}
 	public function Transfer($accountTo,$accountToBankCode,$amount){
 		$Verify = $this->Verify($accountTo,$accountToBankCode,$amount);
@@ -506,6 +514,7 @@ class Scb{
 			"transferType": "'.$Verify['transferType'].'"
 			}';
 		$res = $this->Curl("POST",$url,$headers,$data,false);
+
 		$d = json_decode($res,true);
 
 		if($d['status']['code'] === "1002"){
@@ -605,8 +614,9 @@ class Scb{
 			),
 		));
 		$response = curl_exec($curl);
-		return $response;
 		curl_close($curl);
+		return $response;
+
 	}
 	public function encryptscb($Sid,$ServerRandom,$pubKey,$hashType)
 	{
@@ -668,10 +678,13 @@ class Scb{
 				$headers[$key] = $value;
 			}
 		}
+
+		curl_close($curl);
 		return $headers;
 	}
 	public function new_Login()
 	{
+		//echo "error Login ";
 		$preload = $this->preloadandresumecheck();
 		$e2ee = $this->PseudoFE($preload['Api-Auth']);
 		$e2eejson = json_decode($e2ee,true);
@@ -698,6 +711,5 @@ class Scb{
 
 	}
 	//======================================================
-
 }
 ?>
