446b183b184b6c8112a4d5c343ee380bbb58be2b
rajahugfar <51021914+rajahugfar@users.noreply.github.com>
2023-02-11 20:50:15 +0700

    แก้ kbank

--- lib/Kbank.php
+++ lib/Kbank.php
@@ -19,8 +19,7 @@ class Kbank{
 
 	public function getBalanceAndTransactions(){
 		$curl = curl_init();
-		$fp = fopen('./application/third_party/'.base64_encode("process_kbiz_".$this->acctNo), "w");
-		fclose($fp);
+
 		curl_setopt_array($curl, array(
 			CURLOPT_URL => 'https://online.kasikornbankgroup.com/kbiz/',
 			CURLOPT_RETURNTRANSFER => true,
@@ -28,17 +27,18 @@ class Kbank{
 			CURLOPT_MAXREDIRS => 10,
 			CURLOPT_CONNECTTIMEOUT => 15,
 			CURLOPT_TIMEOUT => 30,
+			CURLOPT_SSL_VERIFYHOST => 0,
+			CURLOPT_SSL_VERIFYPEER => 0,
 			CURLOPT_FOLLOWLOCATION => true,
 			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 			CURLOPT_CUSTOMREQUEST => 'GET',
-			CURLOPT_COOKIEJAR => './application/third_party/'.base64_encode("process_kbiz_".$this->acctNo),
-			CURLOPT_COOKIEFILE => './application/third_party/'.base64_encode("process_kbiz_".$this->acctNo),
+			CURLOPT_COOKIEJAR => './lib/'.base64_encode("process_kbiz_".$this->acctNo),
+			CURLOPT_COOKIEFILE => './lib/'.base64_encode("process_kbiz_".$this->acctNo),
 		));
 
 		$response = curl_exec($curl);
 
 		curl_close($curl);
-
 
 
 		$html = str_get_html($response);
@@ -57,6 +57,7 @@ class Kbank{
 		}
 		$post_string = substr($post_string, 0, -1);
 
+		// print_r($form_field);exit;
 
 		$curl = curl_init();
 		curl_setopt_array($curl, array(
@@ -67,52 +68,76 @@ class Kbank{
 			CURLOPT_MAXREDIRS => 10,
 			CURLOPT_CONNECTTIMEOUT => 15,
 			CURLOPT_TIMEOUT => 30,
+			CURLOPT_SSL_VERIFYHOST => 0,
+			CURLOPT_SSL_VERIFYPEER => 0,
 			CURLOPT_HEADER=> 1,
 			CURLOPT_FOLLOWLOCATION => true,
 			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 			CURLOPT_CUSTOMREQUEST => 'POST',
-			CURLOPT_COOKIEJAR => './application/third_party/'.base64_encode("process_kbiz_".$this->acctNo),
-			CURLOPT_COOKIEFILE => './application/third_party/'.base64_encode("process_kbiz_".$this->acctNo),
+			CURLOPT_COOKIEJAR => './lib/'.base64_encode("process_kbiz_".$this->acctNo),
+			CURLOPT_COOKIEFILE => './lib/'.base64_encode("process_kbiz_".$this->acctNo),
 		));
 
 		$response = curl_exec($curl);
-		$output_array = [];
+
+		// print_r($response);exit;
+
+		$output_array_1 = [];
 		if(strpos($response,"/kbiz/ib/redirectToIB.jsp") !== FALSE){
 			curl_setopt_array($curl, array(
 				CURLOPT_URL => 'https://online.kasikornbankgroup.com/kbiz/ib/redirectToIB.jsp',
 				CURLOPT_CUSTOMREQUEST => 'GET',
-				CURLOPT_COOKIEJAR => './application/third_party/'.base64_encode("process_kbiz_".$this->acctNo),
-				CURLOPT_COOKIEFILE => './application/third_party/'.base64_encode("process_kbiz_".$this->acctNo),
+				CURLOPT_COOKIEJAR => './lib/'.base64_encode("process_kbiz_".$this->acctNo),
+				CURLOPT_COOKIEFILE => './lib/'.base64_encode("process_kbiz_".$this->acctNo),
 			));
 			$response = curl_exec($curl);
-			preg_match_all('/(?<=dataRsso\=).(.*?)(?=\")/', $response, $output_array);
+			preg_match_all('/(?<=dataRsso\=).(.*?)(?=\")/', $response, $output_array_1);
 		}
+
+		// print_r($output_array_1);exit;
+
 		curl_close($curl);
 		$balance = null;
 		$results = [];
-		if(!empty($output_array[0]) && !empty($output_array[0][0])){
+		if(!empty($output_array_1[0]) && !empty($output_array_1[0][0])){
 			$curl = curl_init();
 			curl_setopt_array($curl, array(
+				// CURLOPT_URL => 'https://ib.gateway.kasikornbank.com/api/authentication/validateSession',
 				CURLOPT_URL => 'https://kbiz.kasikornbankgroup.com/services/api/authentication/validateSession',
 				CURLOPT_RETURNTRANSFER => true,
 				CURLOPT_ENCODING => '',
 				CURLOPT_MAXREDIRS => 10,
 				CURLOPT_CONNECTTIMEOUT => 15,
 				CURLOPT_TIMEOUT => 30,
+				CURLOPT_SSL_VERIFYHOST => 0,
+				CURLOPT_SSL_VERIFYPEER => 0,
 				CURLOPT_HEADER=> 1,
 				CURLOPT_FOLLOWLOCATION => true,
 				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 				CURLOPT_CUSTOMREQUEST => 'POST',
-				CURLOPT_POSTFIELDS =>'{"dataRsso":"'.$output_array[0][0].'"}',
+				CURLOPT_POSTFIELDS =>'{"dataRsso":"'.$output_array_1[0][0].'"}',
 				CURLOPT_HTTPHEADER => array(
 					'Content-Type: application/json'
 				),
 			));
 
 			$response = curl_exec($curl);
+
+			// print_r($response);exit;
+
 			$ownerId = substr($response,strpos($response,'"userProfiles":[{"ibId":"')+25);
 			$ownerId = !empty($ownerId) ? explode('","roleList":',$ownerId) : null;
-			$ownerId = !is_null($ownerId) && count($ownerId) >= 2 ? trim($ownerId[0]) : null;
+			$ownerId2 = !is_null($ownerId) && count($ownerId) >= 2 ? trim($ownerId[0]) : null;
+			$checkCompany	= substr($response,strpos($response,'"companyId":'));
+			$checkCompany1	= explode(',',$checkCompany);
+			$checkCompany2	= explode('"',$checkCompany1[0]);
+			if($checkCompany2[3])
+			{
+				$ownerId = $checkCompany2[3];
+			} else {
+				$ownerId = !is_null($ownerId) && count($ownerId) >= 2 ? trim($ownerId[0]) : null;
+			}
+			// print_r($ownerId);exit;
 			if(is_null($ownerId) || empty($ownerId)){
 				echo "Can't find ownerID";
 				exit();
@@ -123,90 +148,74 @@ class Kbank{
 			$ownerType = $custType == "IX" ? "Company" : "Retail";
 			curl_close($curl);
 
-			preg_match_all('/(?<=X\-SESSION\-TOKEN\:).+/', $response, $output_array);
+			preg_match_all('/(?<=x\-session\-token\:).+/', $response, $output_array_2);
 
-
-
-
+			// print_r($output_array_2);exit;
 
 			$curl = curl_init();
 
 			curl_setopt_array($curl, array(
+				// CURLOPT_URL => 'https://ib.gateway.kasikornbank.com/gateway/refreshSession',
 				CURLOPT_URL => 'https://kbiz.kasikornbankgroup.com/services/api/refreshSession',
 				CURLOPT_RETURNTRANSFER => true,
 				CURLOPT_ENCODING => '',
 				CURLOPT_MAXREDIRS => 10,
 				CURLOPT_CONNECTTIMEOUT => 15,
 				CURLOPT_TIMEOUT => 30,
+				CURLOPT_SSL_VERIFYHOST => 0,
+				CURLOPT_SSL_VERIFYPEER => 0,
 				CURLOPT_HEADER=> 1,
 				CURLOPT_FOLLOWLOCATION => true,
 				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 				CURLOPT_CUSTOMREQUEST => 'POST',
 				CURLOPT_POSTFIELDS =>'{}',
 				CURLOPT_HTTPHEADER => array(
-					'Connection:  keep-alive',
-					'X-IB-ID: '.$ownerId,
-					'sec-ch-ua-mobile:  ?0',
-					'Authorization: '.trim($output_array[0][0]),
+					'X-IB-ID: '.$ownerId2,
+					'Authorization: '.trim($output_array_2[0][0]),
 					'Content-Type:  application/json',
-					'X-URL:  https://kbiz.kasikornbankgroup.com/menu/account/account-summary',
-					'Accept:  application/json, text/plain, */*',
-					'User-Agent:  Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.43',
 					'X-RE-FRESH:  N',
 					'X-REQUEST-ID:  '.date('Ymd').'125809248859',
-					'Origin:  https://kbiz.kasikornbankgroup.com',
-					'Sec-Fetch-Site:  cross-site',
-					'Sec-Fetch-Mode:  cors',
-					'Sec-Fetch-Dest:  empty',
-					'Referer:  https://kbiz.kasikornbankgroup.com/',
-					'Accept-Language:  en-US,en;q=0.9,th;q=0.8',
-					'Cookie: 76aa0fb54084497165ebe81fd14b509b=4e17b3927e6157f7d6c7dff277645d12; TS0133a991=014654215437036df3ae0001a00c2db14dda56bdc5c0abc86e06c5e2e15ecf14ded9592d3e851a48e03d3f4eb1280103b389ec0932'
 				),
 			));
 
 			$response = curl_exec($curl);
 			curl_close($curl);
 
-			preg_match_all('/(?<=X\-SESSION\-TOKEN\:).+/', $response, $output_array1);
+			preg_match_all('/(?<=x\-session\-token\:).+/', $response, $output_array_3);
 
+			// print_r($output_array_3);exit;
 
-			if(!empty($output_array1[0]) && !empty($output_array1[0][0])){
+			if(!empty($output_array_3[0]) && !empty($output_array_3[0][0])){
 				$curl = curl_init();
 
 				curl_setopt_array($curl, array(
+					// CURLOPT_URL => 'https://ib.gateway.kasikornbank.com/api/accountsummary/getAccountSummaryList',
 					CURLOPT_URL => 'https://kbiz.kasikornbankgroup.com/services/api/accountsummary/getAccountSummaryList',
 					CURLOPT_RETURNTRANSFER => true,
 					CURLOPT_ENCODING => '',
 					CURLOPT_MAXREDIRS => 10,
 					CURLOPT_CONNECTTIMEOUT => 15,
+					CURLOPT_SSL_VERIFYHOST => 0,
+					CURLOPT_SSL_VERIFYPEER => 0,
 					CURLOPT_TIMEOUT => 30,
 					CURLOPT_FOLLOWLOCATION => true,
 					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 					CURLOPT_CUSTOMREQUEST => 'POST',
 					CURLOPT_POSTFIELDS =>'{"custType":"'.$custType.'","ownerId":"'.$ownerId.'","ownerType":"'.$ownerType.'","nicknameType":"OWNAC","pageAmount":6,"lang":"th","isReload":"N"}',
 					CURLOPT_HTTPHEADER => array(
-						'Connection:  keep-alive',
-						'X-IB-ID: '.$ownerId,
-						'sec-ch-ua-mobile:  ?0',
-						'Authorization: '.trim($output_array1[0][0]),
+						'X-IB-ID: '.$ownerId2,
+						'Authorization: '.trim($output_array_3[0][0]),
 						'Content-Type:  application/json',
-						'X-URL:  https://kbiz.kasikornbankgroup.com/menu/account/account-summary',
-						'Accept:  application/json, text/plain, */*',
-						'User-Agent:  Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.43',
 						'X-RE-FRESH:  N',
 						'X-REQUEST-ID:  '.date('Ymd').'125809248859',
-						'Origin:  https://kbiz.kasikornbankgroup.com',
-						'Sec-Fetch-Site:  cross-site',
-						'Sec-Fetch-Mode:  cors',
-						'Sec-Fetch-Dest:  empty',
-						'Referer:  https://kbiz.kasikornbankgroup.com/',
-						'Accept-Language:  en-US,en;q=0.9,th;q=0.8',
-						'Cookie: 76aa0fb54084497165ebe81fd14b509b=4e17b3927e6157f7d6c7dff277645d12; TS0133a991=014654215437036df3ae0001a00c2db14dda56bdc5c0abc86e06c5e2e15ecf14ded9592d3e851a48e03d3f4eb1280103b389ec0932'
 					),
 				));
 
 				$response = curl_exec($curl);
 				curl_close($curl);
+
+				// print_r($response);exit;
+
 				$data_bal = json_decode($response,true);
 				$balance = null;
 				if(isset($data_bal['data']) && isset($data_bal['data']['accountSummaryList']) && count($data_bal['data']['accountSummaryList']) >= 1){
@@ -230,42 +239,38 @@ class Kbank{
 					}
 					$end_date = date('d/m/Y');
 				}
-
+				// print_r($ownerId2);exit;
 				curl_setopt_array($curl, array(
+					// CURLOPT_URL => 'https://ib.gateway.kasikornbank.com/api/accountsummary/getRecentTransactionList',
 					CURLOPT_URL => 'https://kbiz.kasikornbankgroup.com/services/api/accountsummary/getRecentTransactionList',
 					CURLOPT_RETURNTRANSFER => true,
 					CURLOPT_ENCODING => '',
 					CURLOPT_MAXREDIRS => 10,
 					CURLOPT_CONNECTTIMEOUT => 15,
+					CURLOPT_SSL_VERIFYHOST => 0,
+					CURLOPT_SSL_VERIFYPEER => 0,
 					CURLOPT_TIMEOUT => 30,
 					CURLOPT_FOLLOWLOCATION => true,
 					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 					CURLOPT_CUSTOMREQUEST => 'POST',
 					CURLOPT_POSTFIELDS =>'{"acctNo":"'.$this->acctNo.'","acctType":"SA","custType":"'.$custType.'","ownerType":"'.$ownerType.'","ownerId":"'.$ownerId.'","pageNo":"1","rowPerPage":"35","refKey":"","startDate":"'.$start_date.'","endDate":"'.$end_date.'"}',
 					CURLOPT_HTTPHEADER => array(
-						'Connection:  keep-alive',
-						'X-IB-ID: '.$ownerId,
-						'sec-ch-ua-mobile:  ?0',
-						'Authorization: '.trim($output_array1[0][0]),
-						'Content-Type:  application/json',
-						'X-URL:  https://kbiz.kasikornbankgroup.com/menu/account/account-summary',
 						'Accept:  application/json, text/plain, */*',
-						'User-Agent:  Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.43',
+						'X-IB-ID: '.$ownerId2,
+						'X-SESSION-IBID: '.$ownerId2,
+						'Authorization: '.trim($output_array_3[0][0]),
+						'Content-Type:  application/json',
 						'X-RE-FRESH:  N',
+						'X-URL: https://kbiz.kasikornbankgroup.com/menu/account/account/recent-transaction',
 						'X-REQUEST-ID:  '.date('Ymd').'125809248859',
-						'Origin:  https://kbiz.kasikornbankgroup.com',
-						'Sec-Fetch-Site:  cross-site',
-						'Sec-Fetch-Mode:  cors',
-						'Sec-Fetch-Dest:  empty',
-						'Referer:  https://kbiz.kasikornbankgroup.com/',
-						'Accept-Language:  en-US,en;q=0.9,th;q=0.8',
-						'Cookie: 76aa0fb54084497165ebe81fd14b509b=4e17b3927e6157f7d6c7dff277645d12; TS0133a991=014654215437036df3ae0001a00c2db14dda56bdc5c0abc86e06c5e2e15ecf14ded9592d3e851a48e03d3f4eb1280103b389ec0932'
 					),
 				));
 
 				$response = curl_exec($curl);
 
 				curl_close($curl);
+
+				// print_r($response);exit;
 
 				$data = json_decode($response,true);
 				$curl = null;
@@ -294,44 +299,38 @@ class Kbank{
 						$proxyTypeCode=$value['proxyTypeCode'];
 						$proxyId=$value['proxyId'];
 
-
+						// print_r('{"transDate":"'.$transDate.'","acctNo":"'.$this->acctNo.'","origRqUid":"'.$origRqUid.'","custType":"'.$custType.'","originalSourceId":"'.$originalSourceId.'","transCode":"'.$transCode.'","debitCreditIndicator":"'.$debitCreditIndicator.'","transType":"FTOB","ownerType":"'.$ownerType.'","ownerId":"'.$ownerId.'"}');exit;
 						$curl = curl_init();
 						curl_setopt_array($curl, array(
+							// CURLOPT_URL => 'https://ib.gateway.kasikornbank.com/api/accountsummary/getRecentTransactionDetail',
 							CURLOPT_URL => 'https://kbiz.kasikornbankgroup.com/services/api/accountsummary/getRecentTransactionDetail',
 							CURLOPT_RETURNTRANSFER => true,
 							CURLOPT_ENCODING => '',
 							CURLOPT_MAXREDIRS => 10,
 							CURLOPT_CONNECTTIMEOUT => 15,
+							CURLOPT_SSL_VERIFYHOST => 0,
+							CURLOPT_SSL_VERIFYPEER => 0,
 							CURLOPT_TIMEOUT => 30,
 							CURLOPT_FOLLOWLOCATION => true,
 							CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 							CURLOPT_CUSTOMREQUEST => 'POST',
 							CURLOPT_POSTFIELDS =>'{"transDate":"'.$transDate.'","acctNo":"'.$this->acctNo.'","origRqUid":"'.$origRqUid.'","custType":"'.$custType.'","originalSourceId":"'.$originalSourceId.'","transCode":"'.$transCode.'","debitCreditIndicator":"'.$debitCreditIndicator.'","transType":"FTOB","ownerType":"'.$ownerType.'","ownerId":"'.$ownerId.'"}',
 							CURLOPT_HTTPHEADER => array(
-								'Connection:  keep-alive',
-								'X-IB-ID: '.$ownerId,
-								'sec-ch-ua-mobile:  ?0',
-								'Authorization: '.trim($output_array1[0][0]),
-								'Content-Type:  application/json',
-								'X-URL:  https://kbiz.kasikornbankgroup.com/menu/account/account-summary',
 								'Accept:  application/json, text/plain, */*',
-								'User-Agent:  Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.43',
+								'X-IB-ID: '.$ownerId2,
+								'X-SESSION-IBID: '.$ownerId2,
+								'Authorization: '.trim($output_array_3[0][0]),
+								'Content-Type:  application/json',
 								'X-RE-FRESH:  N',
+								'X-URL: https://kbiz.kasikornbankgroup.com/menu/account/account/recent-transaction',
 								'X-REQUEST-ID:  '.date('Ymd').'125809248859',
-								'Origin:  https://kbiz.kasikornbankgroup.com',
-								'Sec-Fetch-Site:  cross-site',
-								'Sec-Fetch-Mode:  cors',
-								'Sec-Fetch-Dest:  empty',
-								'Referer:  https://kbiz.kasikornbankgroup.com/',
-								'Accept-Language:  en-US,en;q=0.9,th;q=0.8',
-								'Cookie: 76aa0fb54084497165ebe81fd14b509b=4e17b3927e6157f7d6c7dff277645d12; TS0133a991=014654215463e11f513318fc4810039aa2c1b32705e7c48873da499cc22e2ef84f8c6207b723bb28f64aaa9283a3b59671fae1d8ae'
 							),
 						));
 						$response1 = curl_exec($curl);
 						$data1 = json_decode($response1,true);
 						$data['data']['recentTransactionList'][$index]['detail'] = $data1;
-
 						$transaction = $data['data']['recentTransactionList'][$index];
+						// print_r($transaction);
 						$payment_gateway_ext = "";
 						if(isset($transaction['detail']['data']) && isset($transaction['detail']['data']['toAccountNo'])){
 							$toAccountNo = $transaction['detail']['data']['toAccountNo'];
@@ -340,9 +339,18 @@ class Kbank{
 							$set3 = substr($toAccountNo,4,5);
 							$set4 = substr($toAccountNo,9,5);
 							$toAccountNo = $set1.'-'.$set2.'-'.$set3.'-'.$set4;
-							$payment_gateway_ext = $toAccountNo." ".$transaction['detail']['data']['toAccountNameTh']." ".$transaction['detail']['data']['bankNameEn'];
+							$payment_gateway_ext = $toAccountNo;
 						}else{
-							$payment_gateway_ext = $transaction['toAccountNumber']." ".(!empty($transaction['benefitAccountNameTh']) ? $transaction['benefitAccountNameTh'] : $transaction['benefitAccountNameEn']);
+							$payment_gateway_ext = $transaction['toAccountNumber'];
+						}
+						// print_r($transaction['detail']['data']);
+						if($transaction['fromAccountNameEn'] == '')
+						{
+							$transNameTh = $transaction['detail']['data']['toAccountNameTh'];
+							$transNameEn = $transaction['detail']['data']['toAccountNameEn'];
+						} else {
+							$transNameTh = $transaction['fromAccountNameTh'];
+							$transNameEn = $transaction['fromAccountNameEn'];
 						}
 						$results[] = [
 							'date' => trim(explode(" ",$transaction['transDate'])[0]),
@@ -350,7 +358,16 @@ class Kbank{
 							'type' => trim($transaction['channelTh']),
 							'type_deposit_withdraw' => !is_null($transaction['depositAmount']) ? 'D' : 'W',
 							'amount' => !is_null($transaction['depositAmount']) ? $transaction['depositAmount'] : $transaction['withdrawAmount'],
-							'payment_gateway' => trim($transaction['channelTh'].' | '.trim($payment_gateway_ext))
+							'payment_gateway' => trim($payment_gateway_ext),
+							'toAccountNameTh' => trim($transNameTh),
+							'toAccountNameEn' => trim($transNameEn),
+							'bankNameTh' => trim($transaction['detail']['data']['bankNameTh']),
+							'bankNameEn' => trim($transaction['detail']['data']['bankNameEn']),
+							'toAccountNo' => trim($transaction['detail']['data']['toAccountNo']),
+							'toAccountNoMarking' => trim($transaction['detail']['data']['toAccountNoMarking']),
+							'transNameTh' => trim($transaction['transNameTh']),
+							'transNameEn' => trim($transaction['transNameEn']),
+							'memo' => trim($transaction['detail']['data']['memo']),
 						];
 
 					}
@@ -369,7 +386,9 @@ class Kbank{
 			$curl = null;
 			curl_close($curl);
 		}
-		return ['balance'=> $balance,'transactions'=>$results];
+
+
+		return ['balance'=> $balance,'account'=> $this->acctNo,'transactions'=>$results];
 
 	}
 }
